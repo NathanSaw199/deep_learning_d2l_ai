e72ce1872533d78c1fc9a7594c7920fc0904bf73
Joseph Phun Uk Thang <s3922810@student.rmit.edu.au>
2024-12-03 09:43:02 +1100

    Slicing out the 2D array Added a few comment lines to explain the code

--- joseph/2_1.py
+++ joseph/2_1.py
@@ -1,51 +1,90 @@
 
 import torch
 
-# # 1D tensor of 12 elements
-# x = torch.arange(12, dtype=torch.float32)
+# 1D tensor of 12 elements
+x = torch.arange(12, dtype=torch.float32)
 
+# print(x)
+
+# Output: 
+# tensor([ 0.,  1.,  2.,  3.,  4.,  5.,  6.,  7.,  8.,  9., 10., 11.])
+# Each of the values in the output is called an element of the tensor.
+
+# --------------------------------------------------------
+# # We can inspect the total number of elements in a tensor via its <numel> method
 # # can't use the x.numel with x.shape at the same time
 # x = x.numel()
 
+# print(x)
+
+# # Output: 12
+
+# --------------------------------------------------------
+
+# # can't use the x.numel with x.shape at the same time
+
 # y = x.shape
 
-# # matrix of 3 rows and 4 columns
-# # 2D tensor of size 3x4
+# print(y)
+
+# # Output: torch.Size([12])
+
+# --------------------------------------------------------
+
+# # # matrix of 3 rows and 4 columns
+# # # We can reshape the 12 elements into a matrix of 3 rows and 4 columns which is a 2D tensor of size 3x4
 # X = x.reshape(3, 4)
 
-# # 3D tensor of size 2x3x4
-# # torch.zeros creates a tensor of the specified size with all elements set to 0
+# print(X)
+
+# # # 3D tensor of size 2x3x4 ( 2 stands for the number of matrices, 3 stands for the number of rows, and 4 stands for the number of columns)
+# # # torch.zeros creates a tensor of the specified size with all elements set to 0 
 # x = torch.zeros((2, 3, 4))
 
-
+# print(x)
 
 # # 3D tensor of size 2x3x4
 # # torch.ones creates a tensor of the specified size with all elements set to 1
 # x = torch.ones((2, 3, 4))
 
+# # print(x)
+
+# --------------------------------------------------------
 # # torch.randn creates a tensor of the specified size with all elements set randomly
-# # 3x4 matrix with random elements
+# # 2D 3x4 matrix with random elements
 # x = torch.randn(3, 4)
 
 # print(x)
 
-# # 2x3x4 tensor with random elements
+# # 3D 2x3x4 tensor with random elements
 # x = torch.randn(2, 3, 4)
+
+# print(x)
+
 # torch.tensor([[2, 1, 4, 3], [1, 2, 3, 4], [4, 3, 2, 1]])
 
 # print(torch.tensor([[2, 1, 4, 3], [1, 2, 3, 4], [4, 3, 2, 1]]))
 
 # print(y)
 
+# --------------------------------------------------------
+
 # print(f"Before Slicing: {x}")
 
-# # Slicing the last element of the tensor
+# # Slicing the last element of the tensor x[-1]
+# # Select the second and third rows of x x[1:3]
 # print(f"After Slicing: {x[-1], x[1:3]}")
 
-# # Slicing the first element of the tensor
-# print(f"After Slicing: {x[0], x[1:3]}")
+z = torch.tensor([[2, 1, 4, 3], [1, 2, 3, 4], [4, 3, 2, 1]])
 
-# print (f"Orginal Matrices: {x} ")
+print(torch.tensor([[2, 1, 4, 3], [1, 2, 3, 4], [4, 3, 2, 1]]))
+
+# Using alternating indices [0,2] to select first and last rows
+print(f"\n {z[[0,2], :]}")
+
+
+
+# --------------------------------------------------------
 
 # # replace the element at row 1, column 2, with 17
 # X[1,2] = 17
@@ -166,9 +205,9 @@ import torch
 
 # --------------------------------------------------------
 
-a = torch.arange(1, 6).reshape((5, 1))
-b = torch.arange(1, 3).reshape((1, 2))
-print(a, b)
-print(a + b)
+# a = torch.arange(1, 6).reshape((5, 1))
+# b = torch.arange(1, 3).reshape((1, 2))
+# print(a, b)
+# print(a + b)
 
 
