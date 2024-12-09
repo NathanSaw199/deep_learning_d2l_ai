bc136ebdbf165414f76c492bb6adbe6ed0045d4d
NathanSaw199 <104150310@student.swin.edu.au>
2024-12-09 21:53:14 +1100

    competition folder

---
+++ tasks/house_price_prediciton.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+
+# STEP 1 : DOWNLOAD THE DATASET
+
+
+
+
+def download(url,folder,sha1_hash=None):
+    """Download a file to folder and return the local filepath."""
+
+def extract(filename, folder):
+    """Extract a zip/tar file to folder."""
