75578dbd33b59b5eda104e911cbbe44409ae9745
NathanSaw199 <104150310@student.swin.edu.au>
2024-12-11 23:56:57 +1100

    fixed

--- Kaggle_competition/hackerrank1.py
+++ Kaggle_competition/hackerrank1.py
@@ -20,10 +20,10 @@ black_path = root_node + layer2_nodes[1] + goal_nodes[1]
 print("black path:", black_path)
 
 path = {
-    'Blue path': blue_path,
-    'Black path': black_path,
-    'Green path': green_path,
-    'Red path': red_path}
+    'blue': blue_path,
+    'black': black_path,
+    'green': green_path,
+    'red': red_path}
 # print(path.items())
 # print(path.values())
 # print(path.keys())
@@ -32,10 +32,10 @@ path = {
 name, the_highest_value_path = max(path.items(), key=lambda x: x[1])
 # print(the_highest_value_path,name)
 if name == "blue":
-    print(f"The highest value path is blue path and the value is {the_highest_value_path} and the path is {root_node},  {layer2_nodes[0]},{goal_nodes[0]}")
+    print(f"The highest value path is blue path and the value is {the_highest_value_path} and the path is {root_node},{layer2_nodes[0]},{goal_nodes[0]}")
 elif name == "red":
-    print(f"The highest value path is red path and the value is {the_highest_value_path} and the path is {root_node},  {layer2_nodes[0]},{goal_nodes[1]}")
+    print(f"The highest value path is red path and the value is {the_highest_value_path} and the path is {root_node},{layer2_nodes[0]},{goal_nodes[1]}")
 elif name == "green":
-    print(f"The highest value path is green path and the value is {the_highest_value_path} and the path is {root_node},  {layer2_nodes[1]},{goal_nodes[2]}")
+    print(f"The highest value path is green path and the value is {the_highest_value_path} and the path is {root_node},{layer2_nodes[1]},{goal_nodes[2]}")
 elif name == "black":
-    print(f"The highest value path is black path and the value is {the_highest_value_path} and the path is {root_node},  {layer2_nodes[1]},{goal_nodes[1]}")
+    print(f"The highest value path is black path and the value is {the_highest_value_path} and the path is {root_node},{layer2_nodes[1]},{goal_nodes[1]}")
